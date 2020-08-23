c98ba15a3b6d9dfdee28071b45d81a434a40dfc0
jingxu <18843104057@163.com>
2020-08-23 22:41:11 +0800

    23

--- javaExercise/src/InternetEx/test.java
+++ javaExercise/src/InternetEx/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println(Integer.toBinaryString(Integer.highestOneBit(17)));
         System.out.println(Integer.toBinaryString(Integer.lowestOneBit(17)));
+        System.out.println(Integer.reverse(123));
     }
 }
