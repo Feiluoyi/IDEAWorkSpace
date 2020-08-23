20a80d3300918b3c791ef1075dc9b4b79eb98182
jingxu <18843104057@163.com>
2020-08-23 16:08:39 +0800

    23

--- javaExercise/src/InternetEx/test.java
+++ javaExercise/src/InternetEx/test.java
@@ -1,4 +1,8 @@
 package InternetEx;
 
 public class test {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(Integer.highestOneBit(17)));
+        System.out.println(Integer.toBinaryString(Integer.lowestOneBit(17)));
+    }
 }
