6e41aef713071dc1b29e8107b23a6dab2c2723ca
jingxu <18843104057@163.com>
2020-08-25 23:14:06 +0800

    25

--- javaExercise/src/InternetEx/test.java
+++ javaExercise/src/InternetEx/test.java
@@ -2,8 +2,9 @@ package InternetEx;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(Integer.highestOneBit(17)));
-        System.out.println(Integer.toBinaryString(Integer.lowestOneBit(17)));
-        System.out.println(Integer.reverse(123));
+//        System.out.println(Integer.toBinaryString(Integer.highestOneBit(17)));
+//        System.out.println(Integer.toBinaryString(Integer.lowestOneBit(17)));
+//        System.out.println(Integer.reverse(123));
+        System.out.println(Integer.bitCount(7));
     }
 }
